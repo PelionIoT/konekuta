912f0c1ab6cc99be1beb50b65acf4c31b4e84d08
Jan Jongboom <janjongboom@gmail.com>
2016-07-19 16:31:13 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,69 @@
+# Konekuta
+
+A highly-opiniated node.js framework to quickly build dynamic web-applications on top of mbed Device Connector.
+
+The idea behind Konekuta is that for many applications you need to solve the same problems:
+
+* Keeping state consistent, both between Connector and your app, but also between connected clients.
+* Have a fault-tolerant way of sending updates to a device, and keeping the UI sane whenever an update fails.
+* Don't overload Connector by retrieving values from it on every page load.
+
+Konekuta handles this by:
+
+* Loading the total state of all your devices only once, on app startup.
+* Properly handling registrations / de-registrations and notifications from mbed Device Connector.
+* A persistent web socket that syncs state between all connected clients, which will also correct state whenever a client re-connects.
+* Standardized way of handling updates, including callbacks whenever updates fail, so you can show proper feedback to the user.
+
+The framework is not flexible. It makes a bunch of assumptions on your environment and usage which might not fit you. This is by design. If your usecase does not fit Konekuta, use the [mbed Connector node.js library](https://github.com/ARMmbed/mbed-connector-api-node) instead.
+
+## Usage
+
+First add both Konekuta and socket.io to your application:
+
+```
+$ npm install konekuta socket.io --save
+```
+
+You'll also need a web server (like express) and probably a templating library (like hbs) to serve content. You're free to make your own choices here though.
+
+To get started:
+
+```js
+var konekuta = require('konekuta');
+
+konekuta({
+  endpointType: 'Luminaire', // Endpoint type for your devices
+  token: 'aabbcc',           // Access token for Connector
+  io: io,                    // socket.io instance
+  subscribe: {
+    status: '/6002/0/4',     // Which resources to subscribe to. local name, and connector path, will get updated by notifications
+    intensity: '/6002/0/5',
+  },
+  retrieve: {
+    status: '/6002/0/4',    // Which resources to retrieve on app startup (make sure to use same names as under subscribe). Will also be retrieved whenever a new device registers.
+    intensity: '/6002/0/5',
+    ip: 'IPv6/0/Address',
+  },
+  updates: {
+    status: {               // How to update resources. Use the same names as before.
+      method: 'post',       // Automatically creates socket.io routes under 'change-status' which you can write to to update the resource.
+      path: '/6002/0/101',
+    },
+    intensity: {
+      method: 'post',
+      path: '/6002/0/102'
+    },
+  },
+  mapToView: mapToView,     // A function which maps a device -> view model, which you can use in your web app
+  verbose: true,            // Verbose logging
+  dontUpdate: false,        // If true, will not PUT or POST to Connector
+  fakeData: null            // Put an array here with fake details, library will then not actually query Connector. Useful for development.
+
+}, function(devices, ee) {
+   // this would be a good moment to start your web server
+
+   // devices contains all devices, already synced up with device connector
+   // ee is an EventEmitter, which you can use to get access to notifications
+});
+```
---
+++ konekuta.js
@@ -0,0 +1,233 @@
+'use strict';
+
+var CloudApi = require('mbed-connector-api');
+var promisify = require('es6-promisify');
+var co = require('co');
+var assert = require('assert');
+var EventEmitter = require('events');
+
+module.exports = function(options, callback) {
+
+  var ee = new EventEmitter();
+
+  assert.equal(typeof options, 'object', 'Need to pass in options as first argument');
+
+  options.token = process.env.TOKEN || options.token;
+  if (!options.token || options.token === 'YOUR_ACCESS_TOKEN') {
+    console.error('Please set your access token first! (via TOKEN=xxx node main.js, or by pasting it in main.js)');
+    process.exit(1);
+  }
+
+  assert(options.io, 'Need to pass in socket.io instance under options.io');
+  assert(options.endpointType, 'Need to pass in options.endpointType');
+  assert.equal(typeof options.mapToView, 'function', 'Need to pass in options.mapToView');
+  assert.equal(typeof callback, 'function', 'Need to pass in callback as second argument');
+
+  if (options.dontUpdate) {
+    console.log(`Starting in 'dont-update' mode, not syncing changes back to mbed Cloud`);
+  }
+
+  options.subscribe = options.subscribe || {};
+  options.retrieve = options.retrieve || {};
+  options.updates = options.updates || {};
+
+  // This is the truth. This is where we keep state of all connected devices.
+  var devices = [];
+
+  var api = new CloudApi({
+    accessKey: options.token
+  });
+
+  function getResources(endpoint, subscriptions, resourceValues) {
+    return co.wrap(function*() {
+      // we need to run this in series...
+      let ret = {
+        endpoint: endpoint
+      };
+
+      // first subscribe
+      for (let path of subscriptions) {
+        yield promisify(api.putResourceSubscription.bind(api))(endpoint, path);
+        options.verbose && console.log('subscribed to', endpoint, path);
+      }
+
+      // then fix resources
+      for (let key of Object.keys(resourceValues)) {
+        ret[key] = yield promisify(api.getResourceValue.bind(api))(endpoint, resourceValues[key]);
+        options.verbose && console.log('got value', endpoint, key, resourceValues[key], '=', ret[key]);
+      }
+
+      return ret;
+    })();
+  }
+
+  // promisified version of api.getEndpoints
+  function getEndpoints(type) {
+    return new Promise((res, rej) => {
+      api.getEndpoints((err, devices) => {
+        if (err) return rej(err);
+        devices = devices.map(d => {
+          d.endpoint = d.name;
+          return d;
+        });
+        res(devices);
+      }, { parameters: { type: type } });
+    });
+  }
+
+  // Subscribe to resources and get initial values for a device
+  function getDeviceData(endpoint) {
+    let subscribe = Object.keys(options.subscribe).map(k => options.subscribe[k]);
+
+    return getResources(endpoint, subscribe, options.retrieve || {}).catch(err => {
+      options.verbose && console.log('error when retrieving values for', endpoint, err);
+      // don't throw, but rather capture the error...
+      return { err: err };
+    });
+  }
+
+  // Gets the initial batch of devices...
+  function* getDevices() {
+    let devices = yield getEndpoints(options.endpointType);
+
+    options.verbose && console.log('got devices', devices);
+
+    let getDeviceDatas = devices.map(device => getDeviceData(device.endpoint));
+
+    let data = yield Promise.all(getDeviceDatas);
+    options.verbose && console.log('errors:', data.filter(m => m.err));
+
+    // so data is now an array with objects which contains { endpoint, status, intensity, ip }
+    return data;
+  }
+
+  // socket.io
+  options.io.on('connection', function(socket) {
+    // sync all devices to the client to verify that he has latest version
+    let sync = devices.map(d => {
+      return { endpoint: d.endpoint, view: options.mapToView(d) };
+    });
+    socket.emit('device-list', sync);
+
+    for (let name of Object.keys(options.updates)) {
+
+      socket.on('change-' + name, co.wrap(function*(endpoint, newvalue, callback) {
+        console.log('change-' + name, endpoint, newvalue);
+        try {
+          var device = devices.filter(d => d.endpoint === endpoint)[0];
+          if (!device) throw 'Could not find device with endpoint ' + endpoint;
+
+          if (!options.dontUpdate) {
+            let method = options.updates[name].method + 'Resource';
+            yield promisify(api[method].bind(api))(endpoint, options.updates[name].path, newvalue.toString());
+          }
+
+          console.log(`change-${name} OK`, endpoint);
+          device[name] = newvalue;
+          callback();
+        }
+        catch (err) {
+          console.log(`change-${name} failed`, endpoint, err);
+          callback(err.toString());
+        }
+      }));
+
+    }
+
+  });
+
+  // Start notification channel
+  api.startLongPolling(co.wrap(function*(err) {
+    if (err) {
+      console.error('Connection to mbed Cloud failed', err);
+      process.exit(1);
+    }
+
+    console.log('Retrieving initial device model...');
+
+    if (options.fakeData) {
+      devices = options.fakeData;
+    }
+    else {
+      // get all current devices, subscribe to their resources, and retrieve initial values
+      devices = yield getDevices();
+    }
+
+    if (devices.some(d => d.err)) {
+      console.log('Devices with errors:', devices.filter(d => d.err));
+    }
+
+    devices = devices.filter(d => !d.err);
+
+    console.log('Retrieved %d devices on startup', devices.length, devices);
+
+    callback(null, devices, ee);
+  }));
+
+  // Notifications
+  api.on('notification', function(notification) {
+    if (notification.ept !== options.endpointType) return;
+
+    let device = devices.filter(d => d.endpoint === notification.ep)[0];
+    if (!device) {
+      console.error('Got notification for non-existing device...', notification);
+      return;
+    }
+
+    options.verbose && console.log('notification', notification);
+
+    let prop = Object.keys(options.subscribe).filter(k => {
+      return options.subscribe[k] === notification.path;
+    })[0];
+
+    // should not happen but hey
+    if (!prop) return console.error('Notification for non-mapped route...', notification);
+
+    // can intercept from your main app :-)
+    ee.emit('notification-' + prop, device, notification.payload);
+
+    device[prop] = notification.payload;
+    options.io.sockets.emit('change-' + prop, notification.ep, notification.payload);
+
+    options.verbose && console.log('updated device', device);
+  });
+
+  api.on('registration', co.wrap(function*(registration) {
+    if (registration.ept !== options.endpointType) return;
+
+    console.log('new registration', registration, devices);
+
+    let device = devices.filter(d => d.endpoint === registration.ep)[0];
+    if (device) {
+      console.warn('Registration came in for device that already existed...');
+      // so do de-register now, and then register again...
+      api.emit('de-registration', registration);
+    }
+
+    let data = yield getDeviceData(registration.ep);
+
+    devices.push(data);
+
+    console.log('new registration data', data);
+
+    options.io.sockets.emit('created-device', options.mapToView(data));
+  }));
+
+  api.on('de-registration', registration => {
+    if (registration.ept !== options.endpointType) return;
+
+    let device = devices.filter(d => d.endpoint === registration.ep)[0];
+    if (!device) {
+      console.error('de-registration came in for non-existing device...', registration);
+    }
+    devices.splice(devices.indexOf(device), 1);
+
+    console.log('de-registered', registration);
+    options.io.sockets.emit('removed-device', registration.ep);
+  });
+
+  api.on('registration-expired', registration => {
+    console.log('registration-expired happened', registration);
+  });
+
+};
